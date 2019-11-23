f12272e1d6e21be2bc8a1ddcb53d7ed0a79d6509
biruk <forbiruk@gmail.com>
2019-11-23 18:58:55 +0100

    updated resource model

--- milp.h
+++ milp.h
@@ -4,6 +4,6 @@
 #define SIMD_MAX 64
 #define PE_EXP (int) log2(PE_MAX)
 #define SIMD_EXP (int) log2(SIMD_MAX)
-#define TOT_LUT_PYNQ 55637
+#define TOT_LUT_PYNQ 53200
 #define NUM_QUADRANTS 4
 
--- milp_top.cpp
+++ milp_top.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     int i;
-    float percentage_of_resource_used = 0.45;
+    float percentage_of_resource_used = 0.36;
     unsigned int lut = TOT_LUT_PYNQ * percentage_of_resource_used;
     conv_ntwk_arch conv_net[] = {{32, 3, 30, 64, 3},
                                  {30, 64, 28, 64, 3},
@@ -19,6 +19,18 @@ int main()
 
     lfc_ntwk_arch lfc_net[] = {{256, 512}, {512, 512},
                                {512, 64}};
+
+    /*********************************************************************************
+        #LUT(i) = 39.76 * pe(i) + 9.69  * simd(i) + 2244 -- quadrant 1
+        #LUT(i) = 95.13 * pe(i) + 19.79 * simd(i) + 1552 -- quadrant 2
+        #LUT(i) = 19.79 * pe(i) + 96.13 * simd(i) + 1552  -- quadrant 3
+        #LUT(i) = 121.4 * pe(i) + 90.26 * simd(i) - 1674 -- quadrant 4
+    ******************************************************************************/
+    model_variables res_model_lfc[NUM_QUADRANTS] = {{27.3, 6.39, 1570.8}, {67.2, 13.85, 1086.4},
+                                                {13.853, 67.29, 1086}, {84.9, 63.18, -1171.8}};
+
+    model_variables res_model_conv[NUM_QUADRANTS] = {{39.76, 9.69, 2244}, {96.13, 19.79, 1552},
+                                                {19.79, 96.13, 1552}, {121.4, 90.26, -1674}};
 
 #ifdef FIRST_CUT
     const unsigned int num_conv_layer   = 4;
@@ -41,19 +53,6 @@ int main()
         num_ops_per_layer[num_conv_layer + i] = lfc_net[i].matW * lfc_net[i].matH;
         cout << " num_ops " << i << "  " << num_ops_per_layer[num_conv_layer + i] << endl;
     }
-
-    /*********************************************************************************
-        #LUT(i) = 39.76 * pe(i) + 9.69  * simd(i) + 2244 -- quadrant 1
-        #LUT(i) = 95.13 * pe(i) + 19.79 * simd(i) + 1552 -- quadrant 2
-        #LUT(i) = 19.79 * pe(i) + 96.13 * simd(i) + 1552  -- quadrant 3
-        #LUT(i) = 121.4 * pe(i) + 90.26 * simd(i) - 1674 -- quadrant 4
-    ******************************************************************************/
-    model_variables res_model_lfc[NUM_QUADRANTS] = {{39.76, 9.69, 2244}, {96.13, 19.79, 1552},
-                                                {19.79, 96.13, 1552}, {121.4, 90.26, -1674}};
-
-    model_variables res_model_conv[NUM_QUADRANTS] = {{39.76, 9.69, 2244}, {96.13, 19.79, 1552},
-                                                {19.79, 96.13, 1552}, {121.4, 90.26, -1674}};
-
     milp_solver(lut, num_conv_layer, num_lfc_layer, num_ops_per_layer, res_model_lfc, res_model_conv);
 #endif
 
@@ -78,15 +77,7 @@ int main()
         num_ops_per_layer[num_conv_layer + i] = lfc_net[i].matW * lfc_net[i].matH;
         cout << " num_ops " << i << "  " << num_ops_per_layer[num_conv_layer + i] << endl;
     }
-
-    model_variables res_model_lfc[NUM_QUADRANTS] = {{27.3, 6.39, 1570.8}, {67.2, 13.85, 1086.4},     
-                                                {13.853, 67.29, 1086}, {84.9, 63.18, -1171.8}};
-    
-    model_variables res_model_conv[NUM_QUADRANTS] = {{39.76, 9.69, 2244}, {96.13, 19.79, 1552},        
-                                                {19.79, 96.13, 1552}, {121.4, 90.26, -1674}};
-    
-
-    milp_solver(lut, num_conv_layer, num_lfc_layer, num_ops_per_layer, res_model_lfc, res_model_conv);
+   milp_solver(lut, num_conv_layer, num_lfc_layer, num_ops_per_layer, res_model_lfc, res_model_conv);
 #endif
 
 #ifdef FULL
@@ -110,19 +101,6 @@ int main()
         num_ops_per_layer[num_conv_layer + i] = lfc_net[i].matW * lfc_net[i].matH;
         cout << " num_ops " << i << "  " << num_ops_per_layer[num_conv_layer + i] << endl;
     }
-
-        /*********************************************************************************
-        #LUT(i) = 38.89 * pe(i) + 6.101 * simd(i)  + 2365 -- quadrant 1
-        #LUT(i) = 94.9  * pe(i) + 19.66 * simd(i)  + 1629 -- quadrant 2
-        #LUT(i) = 58.52 * pe(i) + 45.58 * simd(i)  + 1454 -- quadrant 3
-        #LUT(i) = 120.7 * pe(i) + 90.29 * simd(i)  - 1606 -- quadrant 4
-    ******************************************************************************/
-    model_variables res_model_lfc[NUM_QUADRANTS] = {{38.89, 6.101, 2365}, {94.9, 19.66, 1629},
-                                                {58.52, 45.58, 1454}, {120.7, 90.29, -1606}};
-
-    model_variables res_model_conv[NUM_QUADRANTS] = {{38.89, 6.101, 2365}, {94.9, 19.66, 1629},
-                                                {58.52, 45.58, 1454}, {120.7, 90.29, -1606}};
-
 
     milp_solver(lut, num_conv_layer, num_lfc_layer, num_ops_per_layer, res_model_lfc, res_model_conv);
 #endif
